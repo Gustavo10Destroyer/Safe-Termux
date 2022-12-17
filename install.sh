c15e4aaeeb5c98942da68915232c205c3b7056ad
Gustavo Vitor Alves <66029954+Gustavo10Destroyer@users.noreply.github.com>
2022-12-17 19:07:16 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,20 @@
+#!/data/data/com.termux/files/usr/bin/bash
 CYAN="\x1b[1;36m"
 GREEN="\x1b[1;32m"
 YELLOW="\x1b[1;33m"
 RESET="\x1b[0m"
+
+if [ -d "$HOME/Safe-Termux" ]; then
+    echo -e "${CYAN}[${YELLOW}!${CYAN}] O '${GREEN}Safe-Termux${CYAN}' está instalado!${RESET}"
+    printf "${CYAN}[${YELLOW}!${CYAN}] Deseja re-instalar o '${GREEN}Safe-Termux${CYAN}'? [${GREEN}s${CYAN}/${YELLOW}N${CYAN}]: ${RESET}"
+    read -r remove
+
+    if [[ "$remove" == "S" || "$remove" == "s" || "$remove" == "Sim" || "$remove" == "sim" || "$remove" == "sIm" || "$remove" == "SiM" || "$remove" == "siM" || "$remove" == "SIm" ]]; then
+        rm -rf "$HOME/Safe-Termux"
+    else
+        exit 0
+    fi
+fi
 
 cd $HOME
 git clone https://github.com/Imperador-RIC/Safe-Termux
