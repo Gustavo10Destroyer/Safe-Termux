0f6a27be6e64437332b223cf440e9e7c8e7c1520
Gustavo10Destroyer <contaderecuperacoes2018@gmail.com>
2022-12-18 12:39:46 -0300

    Bugs corrigidos

--- source/utils.py
+++ source/utils.py
@@ -20,7 +20,8 @@ def print_centered(text: str) -> None:
 
     for index, line in enumerate(text):
         # centralizando o texto na horizontal
-        x = (rows - len(line)) // 2
+        fake_line = re.sub(r"\x1b\[[0-9;]*m", "", line)
+        x = (rows - len(fake_line)) // 2
 
         sys.stdout.write("\x1b[{};{}H".format(y + index, x))
         sys.stdout.write(line)
